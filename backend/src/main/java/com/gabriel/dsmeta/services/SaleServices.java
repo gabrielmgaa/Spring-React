8750b8b756957dcc305f2429b2638b5cdebed5b9
Erzen2912 <gabrielmga2912@gmail.com>
2022-09-10 15:39:21 -0300

    a

--- backend/src/main/java/com/gabriel/dsmeta/services/SaleServices.java
+++ backend/src/main/java/com/gabriel/dsmeta/services/SaleServices.java
@@ -19,13 +19,15 @@ public class SaleServices {
 	private SalesRepository repository;
 
 	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
-		
+
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-		
+
 		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 
-		return repository.findSales(min, max, pageable);
+		return repository.findSales(min, max, pageable); 
 	}
+	
+	
 
 }
